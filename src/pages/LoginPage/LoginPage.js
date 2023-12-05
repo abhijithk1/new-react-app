21c60e0379655aeb67d85e8c29f94f910d875870
Abhijith K <abhijith0807@gmail.com>
2023-12-05 20:48:29 +0530

    login page

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,29 +1,49 @@
 import { Avatar, Button, Flex, Input } from "antd";
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined } from "@ant-design/icons";
 import { Typography } from "antd";
 
-import './LoginPage.css'
+import "./LoginPage.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const { Title, Text } = Typography;
 
 const LoginPage = () => {
-    const navigate = useNavigate();
-    return (
-        <Flex id="box-section" align="center" gap={"middle"} vertical>
-            <Avatar style={{ backgroundColor: '#722ed1' }} icon={<UserOutlined />} size={"large"} />
-            <Title level={3}>Login Page</Title>
-            <Flex id="login-form" justify="center" gap={"large"} vertical>
-                <Input size="large" placeholder="username"/>
-                <Input size="large" placeholder="password" type="password"/>
-                <Button size="large" block type="primary">Login</Button>
-            <Flex align="start" gap={"large"}>
-                <Text underline italic>forgot password?</Text>
-                <Text underline italic onClick={() => navigate("/sign-up")}>Didn't have account? Sign Up</Text>
-            </Flex>
-            </Flex>
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false)
+
+  const loginHandler = () => {
+    setLoading(true);
+    console.log("login")
+    setTimeout(() => {setLoading(false)}, 6000);
+  }
+
+  return (
+    <Flex id="box-section" align="center" gap={"middle"} vertical>
+      <Avatar
+        style={{ backgroundColor: "#722ed1" }}
+        icon={<UserOutlined />}
+        size={"large"}
+      />
+      <Title level={3}>Login Page</Title>
+      <Flex id="login-form" justify="center" gap={"large"} vertical>
+        <Input size="large" placeholder="username" />
+        <Input size="large" placeholder="password" type="password" />
+        <Button size="large" block type="primary" loading={loading} onClick={loginHandler}>
+          Login
+        </Button>
+        <Flex align="start" gap={"large"}>
+          <Text underline italic>
+            forgot password?
+          </Text>
+          <Text underline italic onClick={() => navigate("/sign-up")}>
+            Didn't have account? Sign Up
+          </Text>
         </Flex>
-    );
-}
+      </Flex>
+    </Flex>
+  );
+};
 
 export default LoginPage;
